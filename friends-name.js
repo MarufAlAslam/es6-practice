766c00d647c1e5117f23501f78380698ad7fa770
MarufAlAslam <55714750+MarufAlAslam@users.noreply.github.com>
2022-08-24 22:51:13 +0600

    friends name used spread operator

--- friends-name.js
+++ friends-name.js
@@ -2,6 +2,7 @@ const friends = (names) => {
     const evenLength = names.filter(name => name.length % 2 === 0);
     const evenNames = [...evenLength]
     return evenNames;
+
 }
 
 console.log(friends(['maruf', 'James', 'Jill', 'Jane']))
