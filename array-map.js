4872fce5fbdf7db7995ebf55166997598ba4e7c1
MarufAlAslam <55714750+MarufAlAslam@users.noreply.github.com>
2022-08-27 02:29:51 +0600

    array map done

---
+++ array-map.js
@@ -0,0 +1,5 @@
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+const muliplyByThree = numbers.map(number => number * 3)
+const devideByTwo = numbers.map(number => number / 2)
+console.log(devideByTwo)
