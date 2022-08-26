78cc4a4a39a2326219b12a5396e41ed90daccd7b
MarufAlAslam <55714750+MarufAlAslam@users.noreply.github.com>
2022-08-27 02:28:35 +0600

    nested object done

---
+++ nested-object.js
@@ -0,0 +1,11 @@
+let obj = {
+    name: 'John',
+    age: 30,
+    isMarried: true,
+    wife: {
+        firstWife: 'hannah',
+        secondWife: 'anna'
+    }
+}
+
+console.log(obj.wife.firstWife)
