5c59159619eed3f62fa0994fcf25b8068992dcd3
MarufAlAslam <55714750+MarufAlAslam@users.noreply.github.com>
2022-08-27 05:05:37 +0600

    simple object access done

--- simple-object.js
+++ simple-object.js
@@ -5,3 +5,19 @@ const student = {
 
 const age = student.age
 console.log(age)
+
+
+let data = {
+    location: [
+        {
+            latitude: '34.5, 37.8',
+            longitude: '98.77, 58.7',
+            city: 'Hyderabad',
+            country: 'India'
+        }
+    ]
+}
+
+const data2 = { ...data.location[0] }
+const city = data2.city
+console.log(city)
