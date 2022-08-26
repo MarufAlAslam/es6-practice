d2103e200a96bdf657e118ed1ec6eac24963c913
MarufAlAslam <55714750+MarufAlAslam@users.noreply.github.com>
2022-08-27 03:04:44 +0600

    object value add using array destructuring done

---
+++ object-sum.js
@@ -0,0 +1,8 @@
+const people = [
+    { name: 'meena', age: 20 },
+    { name: 'Rina', age: 15 },
+    { name: 'Tina', age: 22 },
+]
+
+const [meena, rina, tinna] = people
+console.log(meena.age + rina.age + tinna.age)
