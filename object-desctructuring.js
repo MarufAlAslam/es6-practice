8529492e1d1cd74f99839862105028205d160347
MarufAlAslam <55714750+MarufAlAslam@users.noreply.github.com>
2022-08-27 02:43:05 +0600

    object desctructuring done

---
+++ object-desctructuring.js
@@ -0,0 +1,12 @@
+let obj = {
+    name: 'John',
+    age: 30,
+    isMarried: true,
+    gender: 'male',
+}
+
+const { name, age, gender } = obj
+
+console.log(name)
+console.log(age)
+console.log(gender)
